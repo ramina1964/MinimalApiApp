d6ae388e638db6e898f4f6060219aa6b2c646eb0
Ramin Anvar <ramina1964@gmail.com>
2022-08-22 19:10:07 +0200

    Added a C# class called DisplayUsewrModel for presentation of users

---
+++ BlazorServerUI/Data/DisplayUserModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerUI.Data;
+
+public class DisplayUserModel
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "A valid name must have a length in the interval [2, 50] characters.")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "A valid name must have a length in the interval [2, 50] characters.")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Date, ErrorMessage = "DoB must be inside the range [1753-01-01, 1999-12-31].")]
+    public DateTime? DoB { get; set; }
+
+    [EmailAddress]
+    public string? EmailAddress { get; set; }
+}
